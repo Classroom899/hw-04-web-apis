7a081b7fec6d5bcc6bae61e8cb9f135e39c90b54
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-20 16:52:14 -0700

    Add first getElementById and start the quiz questions

--- javascript/js.js
+++ javascript/js.js
@@ -1,3 +1,15 @@
-var test = "Hello World"
+// var test = "Hello World"
 
-console.log("Hello World");
+// console.log("Hello World");
+
+
+// My array of questions for the quiz game and we also need to show the questions in some way or so to say, display them
+var display = document.getElementById("questions");
+var questions 
+
+
+
+
+// Question tracker
+// var  questionTracker = [];
+// var questionAmount = 1;
