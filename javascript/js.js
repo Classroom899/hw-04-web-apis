d847f87c38cdf71125e218aab13e02e730032061
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-22 15:42:49 -0700

    Add question and answer classes

--- javascript/js.js
+++ javascript/js.js
@@ -4,12 +4,24 @@
 
 
 // My array of questions for the quiz game and we also need to show the questions in some way or so to say, display them
-var display = document.getElementById("questions");
-var questions 
+var questionElement = document.getElementById("questions");
+var answersElement = document.getElementById("answers");
+var questions = [
+    { q: "What is a string?", a: "A string is any grouping of characters surrounded by single or can be double quotes"},
+    { q: "What does null represent?", a: "Null represents the intentional absence of value."},
+    { q: "Is JavaScript"}
+
+
+]
+
+// Loop over every question object
+for (var i = 0; i < questions.length; i++) {
+    // Display current question to user and ask 
+}
 
 
 
 
 // Question tracker
-// var  questionTracker = [];
-// var questionAmount = 1;
+var  questionTracker = [];
+var questionAmount = 1;
