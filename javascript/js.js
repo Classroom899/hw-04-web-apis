fdba50bd794fa4b8af9ee46425eeaa276570d6f7
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-23 12:10:43 -0700

    Change up question format for a new idea

--- javascript/js.js
+++ javascript/js.js
@@ -6,22 +6,47 @@
 // My array of questions for the quiz game and we also need to show the questions in some way or so to say, display them
 var questionElement = document.getElementById("questions");
 var answersElement = document.getElementById("answers");
-var questions = [
-    { q: "What is a string?", a: "A string is any grouping of characters surrounded by single or can be double quotes"},
-    { q: "What does null represent?", a: "Null represents the intentional absence of value."},
-    { q: "Is JavaScript"}
+var answersLeft = document.getElementById("answers-left");
+var answersRight = document.getElementById("answers-right");
 
+var questions = [
+    {
+        prompt: "What is a string?\n(A) Any grouping of characters surrounded by single or can be double quotes\n\ (B) A place\n\(C) An inner HTML element\n\(D) A new function",
+        answer: "A"
+    },
+    {
+        prompt: "What does null represent?\n(A) New game coming out\n\ (B) The intentional absence of value\n\(C) A new idea\n\(D) An SQL element",
+        answer: "B"
+    },
+    {
+        prompt: "What kind of number does Math.random return?\n\(A) An integer\n\ (B) A floating-point\n\(C) A whole number",
+        answer: "B"
+    }
 
 ]
 
-// Loop over every question object
-for (var i = 0; i < questions.length; i++) {
-    // Display current question to user and ask 
-}
+
+
+
+
+// { q: "Is JavaScript"}
+
+
 
 
 
 
 // Question tracker
-var  questionTracker = [];
+var questionTracker = [];
 var questionAmount = 1;
+
+// Loop over every question object and iterate however many times you want
+for (var i = 0; i < questionsAmount; i++) {
+    // Display current question to user and ask
+    do {
+        randomQuestion = math.Floor(Math.random() * questionElement)
+    } while (existingQuestions());
+
+    display.innerHTML += questions[randomQuestion].a;
+
+}
